1f7e5b6fe9f5f6e2bbf4add9572189c26d51cf8a
iosmankovi2 <iosmankovi2@etf.unsa.ba>
2020-11-10 11:46:48 +0100

    Klasa TelefonskiBroj

---
+++ src/ba/unsa/etf/rpr/tutorijal_3/TelefonskiBroj.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.rpr.tutorijal_3;
+
+public abstract class TelefonskiBroj implements Comparable<TelefonskiBroj>{
+
+    public abstract String ispisi();
+
+    @Override
+    public abstract  int hashCode();
+
+    @Override
+    public int compareTo(TelefonskiBroj telefonskiBroj) {
+
+        return this.ispisi().compareTo(telefonskiBroj.ispisi());
+
+    }
+}
