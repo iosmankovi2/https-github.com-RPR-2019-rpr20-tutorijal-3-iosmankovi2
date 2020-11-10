01b49f6babef397ca47edbc2354ee60aaed4a657
iosmankovi2 <iosmankovi2@etf.unsa.ba>
2020-11-10 13:49:25 +0100

    Klasa FiksniBroj - implementacija

--- src/ba/unsa/etf/rpr/tutorijal_3/FiksniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal_3/FiksniBroj.java
@@ -2,16 +2,6 @@ package ba.unsa.etf.rpr.tutorijal_3;
 
    public class FiksniBroj extends TelefonskiBroj {
 
-       @Override
-       public String ispisi() {
-           return null;
-       }
-
-       @Override
-       public int hashCode() {
-           return 0;
-       }
-
        public enum Grad {
         TUZLA, SARAJEVO, ZENICA, TRAVNIK, ORASJE, LIVNO,
         MOSTAR, BIHAC, GORAZDE, SIROKIBRIJEG, BRCKO,
@@ -39,4 +29,90 @@ package ba.unsa.etf.rpr.tutorijal_3;
 
     }
 
+    public FiksniBroj(Grad grad, String broj) {
+        this.grad = grad;
+        this.broj = broj;
+    }
+
+       @Override
+       public String ispisi() {
+           String ispisi="";
+           switch(grad)
+           {
+               case TUZLA:
+                   ispisi += "035/";
+                   break;
+               case SARAJEVO:
+                   ispisi += "033/";
+                   break;
+               case ZENICA:
+                   ispisi += "032/";
+                   break;
+               case TRAVNIK:
+                   ispisi += "030/";
+                   break;
+               case ORASJE:
+                   ispisi += "031/";
+                   break;
+               case LIVNO:
+                   ispisi += "034/";
+               case MOSTAR:
+                   ispisi += "036/";
+                   break;
+               case BIHAC:
+                   ispisi += "037/";
+                   break;
+               case GORAZDE:
+                   ispisi += "038/";
+                   break;
+               case SIROKIBRIJEG:
+                   ispisi += "039";
+                   break;
+               case BRCKO:
+                   ispisi += "049";
+                   break;
+               case MRKONJICGRAD:
+                   ispisi += "050/";
+                   break;
+               case BANJALUKA:
+                   ispisi += "051/";
+                   break;
+               case PRIJEDOR:
+                   ispisi += "052/";
+                   break;
+               case DOBOJ:
+                   ispisi += "053/";
+                   break;
+               case SAMAC:
+                   ispisi += "054/";
+                   break;
+               case BIJELJINA:
+                   ispisi += "055/";
+                   break;
+               case ZVORNIK:
+                   ispisi += "056/";
+                   break;
+               case PALE:
+                   ispisi += "057/";
+                   break;
+               case FOCA:
+                   ispisi += "058/";
+                   break;
+               case TREBINJE:
+                   ispisi += "059/";
+                   break;
+
+               default:
+                   ispisi += "?";
+                   break;
+
+           }
+           ispisi += broj;
+           return  ispisi;
+       }
+
+       @Override
+       public int hashCode() {
+           return 0;
+       }
 }
