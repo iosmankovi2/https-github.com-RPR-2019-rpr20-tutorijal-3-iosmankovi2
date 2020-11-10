71aa97ad3f6db8dde1fe46d8501580943efc62e7
iosmankovi2 <iosmankovi2@etf.unsa.ba>
2020-11-10 14:01:13 +0100

    Izvedena klasa2

---
+++ src/ba/unsa/etf/rpr/tutorijal_3/MedunarodniBroj.java
@@ -0,0 +1,25 @@
+package ba.unsa.etf.rpr.tutorijal_3;
+
+public class MedunarodniBroj extends TelefonskiBroj {
+
+    private String drzava;
+    private String broj;
+
+    // konstruktor
+     public MedunarodniBroj(String drzava, String broj) {
+         this.drzava = drzava;
+         this.broj = broj;
+     }
+
+     @Override
+
+     public int hashCode() {
+         return  0;
+     }
+
+     @Override
+
+     public String ispisi() {
+         return ""+drzava + broj;
+     }
+}
